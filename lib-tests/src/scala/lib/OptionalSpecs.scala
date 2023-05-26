43dfef7304b64ebbac7efc675461a55cfff31dae
wahtique <williamvphan@yahoo.fr>
2023-05-26 15:45:49 +0200

    test: tst optional using munit

---
+++ lib-tests/src/scala/lib/OptionalSpecs.scala
@@ -0,0 +1,16 @@
+package lib
+
+import munit.FunSuite
+import optional.*
+
+class OptionalSpecs extends FunSuite:
+
+  test("traverse OK list of int as string => Some(list as int)"):
+    val result = optional:
+      List("1", "2", "3").map(_.toIntOption.?)
+    assertEquals(result, Some(List(1, 2, 3)))
+
+  test("traverse KO list of int as string => None"):
+    val result = optional:
+      List("1", "2", "3", "a").map(_.toIntOption.?)
+    assertEquals(result, None)
